cc14efbad85a74d36a36a169e565b8e8bdc673b9
rossliam2212 <rossliam2212@gmail.com>
2025-07-18 20:08:25 +0100

    Added exceptions.py

---
+++ crypto_fetch/exceptions.py
@@ -0,0 +1,25 @@
+class APIKeyError(Exception):
+    """
+    Exception for API key releated errors.
+
+    :param message: The exception message.
+    """
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self):
+        return self.message
+
+class APIResponseError(Exception):
+    """
+    Exception for API response related errors.
+
+    :param message: The exception message.
+    """
+    def __init__(self, message: str, status_code: int):
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self):
+        return self.message
